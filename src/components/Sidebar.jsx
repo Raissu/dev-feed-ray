f91b6cd50c64b1f1651beb7ddbd877b3ffe76ab9
Kuster <kustermendes10@gmail.com>
2023-05-02 02:51:54 -0300

    feat: finished sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import frame from "../assets/Frame.svg";
+import { PencilLine } from "phosphor-react";
 
 export function Sidebar() {
   return (
@@ -9,14 +9,21 @@ export function Sidebar() {
       />
 
       <div className="flex flex-col items-center">
-        <strong>Raysson Mendes</strong>
-        <span className="text-gray-400">Fullstack Dev</span>
+        <img
+          className="box-border w-12 h-12 border-4 mt-[-1.5rem]  border-solid outline-2 outline-none outline-black border-green-feed rounded-lg"
+          src="https://github.com/raissu.png"
+        />
+
+        <strong className="mt-4 leading-none">Raysson Mendes</strong>
+        <span className="text-gray-400 mt-1 text-[0.875rem]">
+          Fullstack Dev
+        </span>
       </div>
 
       <footer className="mt-6 py-6 px-8 pb-8 border-t-[1px] border-solid border-gray-500 text-green-feed">
-        <button className="flex justify-center items-center w-full h-[50px] py-0 px-6 font-bold bg-transparent border-x-[1px] border-y-[1px] border-green-feed rounded-lg">
-          <img src={frame} alt="editor" />
-          <strong>Editar seu perfil</strong>
+        <button className="hover:text-white transtion duration-500 hover:bg-green-feed gap-[0.5rem] flex justify-center items-center w-full h-[50px] py-0 px-6 font-bold bg-transparent border-x-[1px] border-y-[1px] border-green-feed rounded-lg">
+          <PencilLine />
+          <p>Editar seu perfil</p>
         </button>
       </footer>
     </aside>
